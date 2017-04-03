cd21ff2ec4036a3ad269b2c93a18492fbd3ec1df
dianaonrails <d.r.carvalho89@gmail.com>
2017-04-03 21:19:48 +0100

    remove password compatibility

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-    require_once("libraries/password_compatibility_library.php");
+    //require_once("libraries/password_compatibility_library.php");
 }
 
 
